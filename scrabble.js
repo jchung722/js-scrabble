f61524599aae41b0f4b2f5a6410b73a15c96585b
Jessica <jchung722@gmail.com>
2016-11-15 15:42:30 -0800

    added node modules folder

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,43 @@
-var Scrabble = function() {};
+'use strict';
+var prompt = require('prompt');
 
-// YOUR CODE HERE
-Scrabble.prototype.helloWorld = function() {
-  return 'hello world!';
+prompt.start();
+
+prompt.get(['word'], wordEntry);
+
+var letters = {
+  "AEIOULNRST": 1,
+  "DG": 2,
+  "BCMP": 3,
+  "FHVWY": 4,
+  "K": 5,
+  "JX": 8,
+  "QZ": 10
 };
 
-module.exports = Scrabble;
+var Scrabble = function() {
+  this.score = function wordEntry(err, result) {
+    var total = 0;
+    var word = result.word.toUpperCase();
+    for (var i = 0; i < word.length; i++) {
+      for (var group in letters) {
+        if (group.includes(word[i])) {
+          total += letters[group];
+          return total;
+        }
+      }
+    }
+  };
+
+  Scrabble.score();
+  // this.highestScoreFrom = function (arrayOfWords) {
+  //
+  // }
+};
+
+// YOUR CODE HERE
+// Scrabble.prototype.helloWorld = function() {
+//   return 'hello world!';
+// };
+//
+// module.exports = Scrabble;
